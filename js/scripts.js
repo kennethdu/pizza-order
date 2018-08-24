25a7eacd0b6d8215f2df24e8cefb4888d1722e6c
Kenneth Du <kennethdu3@gmail.com>
2018-08-24 12:57:00 -0700

    add additional placeholder html for user to input name and address in forms given and some supplemental javascript. not fully functional

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,26 @@ $(document).ready(function(){
     var cheese = $("#cheese").val();
     var sauce = $("#sauce").val();
     var meat= $("#meat").val();
-    var newPizza = new pizza(size, cheese, sauce, cheese);
+    var newPizza = new pizza(size, cheese, sauce, meat);
     var newPrice = calculatePrice(newPizza);
-    // alert(newPrice);
-    $("#pizzaOrder").append("<li>" + size   + cheese  + sauce  + meat + "</li>");
+    $("#pizzaOrder").append("<p>" + newPizza.order() + "</p>")
     $("#pizzaPrice").text(newPrice);
   });
 });
 
 //BACKEND
-var ingredientPrices = {"small":10, "medium":14, "large":18, "noCheese":0, "avgCheese":4, "extraCheese":6, "bbq":2, "sauce":2, "alfredo":2, "chicken":3, "pepperoni":2, "sardines":4}
+var ingredientPrices = {"small":10, "medium":14, "large":18, "no cheese":0, "average cheese":4, "extra cheese":6, "bbq":2, "sauce":2, "alfredo":2, "chicken":3, "pepperoni":2, "sardines":4}
 var total = 0;
 
-function pizza(size, cheese, sauce, meat, price) {
+function pizza(size, cheese, sauce, meat) {
   this.size = size;
   this.cheese = cheese;
   this.sauce = sauce;
   this.meat = meat;
+}
+
+pizza.prototype.order = function() {
+  return ("You ordered a " + this.size + ", " + this.cheese + ", " + this.sauce + ", " + this.meat + " pizza")
 }
 
 function calculatePrice(price) {
