74e1af57b5eed09e93b94af060967ebf3fd5a853
Kenneth Du <kennethdu3@gmail.com>
2018-08-24 09:51:33 -0700

    add function for pizza cost calculations

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,34 @@
+//FRONTEND
 $(document).ready(function(){
   $("form#orderSheet").submit(function(event){
     event.preventDefault();
-    var size = $("#size").val();
-    var cheese = $("#cheese").val();
-    var sauce = $("#sauce").val();
-    var meat= $("#meat").val();
-    $("#pizzaOrder").append("<li>" + size + "</li>" + "<li>" + cheese + "</li>" + "<li>" + sauce + "</li>" + "<li>" + meat + "</li>");
-
+    var inputSize = $("#size").val();
+    var inputCheese = $("#cheese").val();
+    var inputSauce = $("#sauce").val();
+    var inputMeat= $("#meat").val();
+    var newPizza = new pizza(inputSize, inputCheese, inputSauce, inputCheese);
+    var newPrice = calculatePrice(newPizza);
+    alert(newPrice);
+    $("#pizzaOrder").append("<li>" + inputSize + "</li>" + "<li>" + inputCheese + "</li>" + "<li>" + inputSauce + "</li>" + "<li>" + inputMeat + "</li>");
   });
 });
+
+//BACKEND
+
+function pizza(size, cheese, sauce, meat, price) {
+  this.size = size;
+  this.cheese = cheese;
+  this.sauce = sauce;
+  this.meat = meat;
+  // this.price = 0;
+}
+
+function calculatePrice(price) {
+  var ingredientPrices = {"small":10, "medium":14, "large":18, "noCheese":0, "avgCheese":4, "extraCheese":6, "bbq":2, "sauce":2, "alfredo":2, "chicken":3, "pepperoni":2, "sardines":4}
+  var total = 0;
+  var inputArray = Object.values(price);
+  inputArray.forEach(function(totals){
+    total = total + ingredientPrices[totals];
+  });
+  return total
+};
