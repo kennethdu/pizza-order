aa2a2ae12d2d8bfd1d00ec863496847d4c712beb
Kenneth Du <kennethdu3@gmail.com>
2018-08-24 13:39:36 -0700

    finished logic of project. working on styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,33 @@
 $(document).ready(function(){
   $("form#orderSheet").submit(function(event){
     event.preventDefault();
+    $("#hideResults").show();
     var size = $("#size").val();
     var cheese = $("#cheese").val();
     var sauce = $("#sauce").val();
     var meat= $("#meat").val();
     var newPizza = new pizza(size, cheese, sauce, meat);
-    var newPrice = calculatePrice(newPizza);
+    // var newPrice = calculatePrice(newPizza);
     $("#pizzaOrder").append("<p>" + newPizza.order() + "</p>")
-    $("#pizzaPrice").text(newPrice);
+    $("#pizzaPrice").text(newPizza.calculatePrice());
+    $("#orderSheet").hide();
+  })
+  $("form#nameForm").submit(function(event){
+    event.preventDefault();
+    var name = $("#name").val();
+    var address= $("#address").val();
+    $("#submission").text("Thank you for ordering " + name + "we will deliver shortly to " + address);
   });
 });
 
 //BACKEND
-var ingredientPrices = {"small":10, "medium":14, "large":18, "no cheese":0, "average cheese":4, "extra cheese":6, "bbq":2, "sauce":2, "alfredo":2, "chicken":3, "pepperoni":2, "sardines":4}
-var total = 0;
+// var ingredientPrices = {"small":10, "medium":14, "large":18, "no cheese":0, "average cheese":4, "extra cheese":6, "bbq":2, "sauce":2, "alfredo":2, "chicken":3, "pepperoni":2, "sardines":4}
+// var total = 0;
+
+var inputSize = {"small":10, "medium":14, "large":18};
+var inputCheese = {"no cheese":1, "average cheese":4, "extra cheese":6};
+var inputSauce = {"bbq":2, "sauce":2, "alfredo":2};
+var inputMeat = {"chicken":3, "pepperoni":2, "sardines":4};
 
 function pizza(size, cheese, sauce, meat) {
   this.size = size;
@@ -25,13 +38,17 @@ function pizza(size, cheese, sauce, meat) {
 }
 
 pizza.prototype.order = function() {
-  return ("You ordered a " + this.size + ", " + this.cheese + ", " + this.sauce + ", " + this.meat + " pizza")
+  return ("a " + this.size + ", " + this.cheese + ", " + this.sauce + ", " + this.meat + " pizza")
 }
 
-function calculatePrice(price) {
-  var inputArray = Object.values(price);
-  inputArray.forEach(function(totals){
-    total = total + ingredientPrices[totals];
-  });
-  return total
-};
+pizza.prototype.calculatePrice = function ()
+{
+  return inputSize[this.size] + inputCheese[this.cheese] + inputSauce[this.sauce] + inputMeat[this.meat]
+}
+// function calculatePrice(price) {
+//   var inputArray = Object.values(price);
+//   inputArray.forEach(function(totals){
+//     total = total + ingredientPrices[totals];
+//   });
+//   return total
+// };
